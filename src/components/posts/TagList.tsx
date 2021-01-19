f500d4ae3bb4dab2760aa7de5adfc438bc32495e
MIE999 <kei.west.post920@gmail.com>
2021-01-19 16:21:51 +0900

    improved design of TagList component

--- src/components/posts/TagList.tsx
+++ src/components/posts/TagList.tsx
@@ -6,22 +6,38 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   wrapper: {
-    padding: '.5rem 0',
+    padding: '.5rem',
     backgroundColor: 'white',
     borderRadius: '.2rem',
     boxShadow: '0 .2rem .5rem black',
   },
-  tagList: {
+  tagListWrapper: {
+    margin: '.3rem',
     '& > p': {
       fontWeight: '900',
       fontSize: '1.3rem',
-      margin: '.2rem .5rem',
+      margin: '.2rem 0rem .5rem',
     },
-    '& > ul': {
-      margin: '0 auto',
-      '& > li': {
-        lineHeight: '1.5rem',
+  },
+  tagList: {
+    alignItems: 'center',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    '& > span': {
+      margin: '0 .5rem .5rem 0',
+      padding: '0 .5rem 0 .5rem',
+      lineHeight: '1.5rem',
+      backgroundColor: '#ebebeb',
+      borderRadius: '.3rem',
+      '& > a': {
+        fontSize: '.8rem',
         textDecoration: 'none',
+        color: '#3273dc',
+        '&:hover': {
+          color: '#4a4a4a',
+          cursor: 'pointer',
+        },
       },
     },
   },
@@ -36,15 +52,15 @@ const TagList: FC = () => {
   return (
     <>
       <aside className={classes.wrapper}>
-        <div className={classes.tagList}>
+        <div className={classes.tagListWrapper}>
           <p>topics</p>
-          <ul>
+          <div className={classes.tagList}>
             {tags.map((tag) => (
-              <li>
-                <Link to={`/posts/tags/${tag}`}>{tag}</Link>
-              </li>
+              <span>
+                <Link to={`/posts/tags/${tag}`}>#{tag}</Link>
+              </span>
             ))}
-          </ul>
+          </div>
         </div>
       </aside>
     </>
